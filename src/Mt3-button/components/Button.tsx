c1ac5df1dfa5ea0773f66ddd307f6c377bc71861
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-02 16:23:58 +0300

    add button style Mt3 Lesson

--- src/Mt3-button/components/Button.tsx
+++ src/Mt3-button/components/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from '../Mt3.module.css';
 
 
 type ButtonType = {
@@ -14,7 +15,7 @@ export const Button = (props: ButtonType) => {
     }
 
     return (
-        <div>
+        <div className={s.button}>
             <button onClick={onClickHandler}>{props.name}</button>
         </div>
     )
